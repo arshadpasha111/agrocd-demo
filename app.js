61d2a367f5610138c807c8cb8c16eb313f2c0f22
arshadpasha111 <31982859+arshadpasha111@users.noreply.github.com>
2025-12-11 02:36:40 +0530

    app.js
    v10

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello ArgoCD!'));
+app.get('/', (req, res) => res.send('Welcome to ArgoCD!'));
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
 
